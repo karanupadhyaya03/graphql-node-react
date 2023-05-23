a9feb7ebc41c3b36a49ee23509f1564bff008c76
Karan Upadhyaya <karanupadayaya2003@gmail.com>
2023-05-23 23:58:44 +0530

    final commit

---
+++ server/models/book.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  name: String,
+  genre: String,
+  author: String,
+  authorId: String,
+});
+
+module.exports = mongoose.model("Book", bookSchema);
